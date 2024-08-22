935087352fc968386ab99941ef527523dafa7f54
j2.jeremias <j2.jeremias@gmail.com>
2024-08-22 16:05:49 +1000

    using localStorage, app lifecycle hooks

--- scripts.js
+++ scripts.js
@@ -26,10 +26,17 @@ const todosApp = {
                 this.newTodo = {
                     done: false
                 };
+                localStorage.setItem("todos", JSON.stringify(this.todos));
             } else {
                 alert("To-do text is required")
             }
+        },
+        storeTodos() {
+            localStorage.setItem("todos", JSON.stringify(this.todos));
         }
+    },
+    created() {
+        this.todos = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : this.todos;
     }
 };
 
